e28f58e41179d17a07d14ce7924fccff21671128
DevErmak <o.khasanov.dev@gmail.com>
2023-08-29 13:22:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Todoform from './components/Todoform/Todoform'
+
+// let item = list.map((li)=> <li> {li.id} </li>)
+
+function App() {
+  return (
+    <>
+     <Todoform />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Todoform/Todoform.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import './todoform.css'
+import Task from '../Task/Task'
+
+
+
+export default function Todoform() {
+  
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  )
+
+  const [inputTask, setInputTask] = useState('')
+
+  const [foundTasks, setFoundTask] = useState([])
+
+  const [isSearch, setIsSearch] = useState(false)
+
+  const [groupTasks, setGroupTasks] = useState([])
+
+
+
+
+  const addTask = () =>{
+    if(inputTask.trim() !== ''){
+      setTasks([...tasks, {id: Math.random(new Date()), title: inputTask, isComplited: false}])
+      setInputTask('')
+      setFoundTask([])
+      setIsSearch(false)
+    }
+
+  }
+
+  const addTaskEnter = (e) =>{
+    if(e.code === 'Enter'){
+      addTask()
+    }
+  }
+
+
+  const changeTodo = (id) => {
+    const copy = [...tasks]
+    const currentObjTask = copy.find(t => t.id === id)
+    currentObjTask.isComplited = !currentObjTask.isComplited
+    setTasks(copy)
+  }
+
+  const removeOneTask = (id) => {
+    setTasks([...tasks].filter(t => t.id !==id))
+    setFoundTask([...foundTasks].filter(t => t.id !==id))
+  }
+
+  const findTasks = () =>{
+    if(inputTask.trim() === '' || foundTasks === [] ){
+      setIsSearch (false)
+    } 
+    else {
+      setIsSearch (!isSearch)
+    }
+    setFoundTask([...tasks].filter(t => t.title === inputTask))
+  }
+
+  const removeGroupTasks = () =>{
+    setTasks([...tasks].filter(t => t.isComplited === false))
+    setFoundTask([...foundTasks].filter(t => t.isComplited === false))
+  }
+  
+  useEffect(()=>{
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    setGroupTasks([...tasks].filter(t => t.isComplited === true))
+  },[tasks])
+
+  return (
+    <>
+    <div className='todoform'>
+    <input value={inputTask} type='text' onKeyUpCapture={(e)=>addTaskEnter(e)}
+    placeholder='введите задачу'
+    onChange={(e)=>setInputTask(e.target.value)}></input>
+    <button onClick={()=>addTask()}>Записать</button>
+    <button onClick={()=>findTasks()}>{isSearch && inputTask.trim() !== '' ? "закрыть поиск" : "поиск"}</button>
+    <button onClick={()=>removeGroupTasks()} 
+    style={{visibility: groupTasks.length > 1 ? null : 'hidden'  }}>удалить группу элементов</button>
+
+    { isSearch && inputTask.trim() !== '' ? foundTasks === [] ? console.log(isSearch): foundTasks.map(foundTask => <Task key={foundTask.id} task={foundTask} changeTodo={changeTodo} removeOneTask={removeOneTask}/>) :
+      tasks.map(task => <Task key={task.id} task={task} changeTodo={changeTodo} removeOneTask={removeOneTask}/>)}
+    
+    </div>
+    </>
+  ) 
+}
